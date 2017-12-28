cd8d26ac3007fd40578ce97adbb92d7f39e5d7a1
David García <ccristhiangarcia@gmail.com>
2017-12-28 16:48:34 -0500

    integrate Bootstrap 3 in Laravel 5

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                
+                <div class="panel panel-default">
+                    <div class="panel-heading">Welcome!</div>
+                    <div class="panel-body">
+                        <h1>Laravel 5</h1>
+                        <p>Welcome to our site!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
