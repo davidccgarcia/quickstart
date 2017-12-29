f31037fd068a126e72abdd37f7ce2ec23a064257
David García <ccristhiangarcia@gmail.com>
2017-12-29 08:49:48 -0500

    Password recovery

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -34,3 +34,11 @@ Route::get('register', [
     'as' => 'register'
 ]);
 Route::post('register', 'Auth\AuthController@postRegister');
+
+// Password reset link request routes...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset routes...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
